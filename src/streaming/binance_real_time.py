eaa4beee683f9166cdcb04592e36497c0e40112b
202304626 <202304626@alu.comillas.edu>
2026-03-12 18:51:02 +0100

    first attempt

--- src/streaming/binance_real_time.py
+++ src/streaming/binance_real_time.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 from binance import Client
 from binance import ThreadedWebsocketManager
+from kafka_simple_producer import produce
 
 def handle_kline(msg):
     k = msg['k']
-    print(
-        f"Intervalo: {k['i']} | "
-        f"Cierre: {k['c']} | "
-        f"Volumen: {k['v']}"
-    )
+
+    if k["x"]:  # Solo producimos si ha cerrado la vela (lo indica el booleano de k["x"])
+        produce(data=k)
+
 
 twm = ThreadedWebsocketManager()
 twm.start()
 
 twm.start_kline_socket(
-    symbol='BTCUSDT',
+    symbol='SOLBTC',
     interval=Client.KLINE_INTERVAL_1MINUTE,
     callback=handle_kline
 )
 
 input("Pulsa ENTER para salir\n")
-twm.stop()
+tw
